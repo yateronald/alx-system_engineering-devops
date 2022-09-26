8af5eda8363db44a44c2a65167fc5d282e88334f
yateronald <yateronald@gmail.com>
2022-09-25 17:18:26 -0700

    web server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,7 +1,7 @@
 # install nginx
 exec {'/usr/bin/env apt-get -y update': }
 exec {'/usr/bin/env apt-get -y install nginx': }
-exec {'/usr/bin/env echo "Holberton School" > /var/www/html/index.nginx-debian.html': }
+exec {'/usr/bin/env echo "Hello World!" > /var/www/html/index.nginx-debian.html': }
 exec {'/usr/bin/env sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.holbertonschool.com permanent;" /etc/nginx/sites-available/default': }
 exec {'/usr/bin/env sed -i "/server_name _;/ a\\\terror_page 404 /custom_404.html;" /etc/nginx/sites-available/default': }
 exec {'/usr/bin/env echo "Ceci n\'est pas une page" > /var/www/html/custom_404.html': }
